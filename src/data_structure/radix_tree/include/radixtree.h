5dfbce84f8064f167064bf474cae415f8938c2f3
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-08-03 21:51:56 +0900

    add functions for managing radix tree operations

--- src/data_structure/radix_tree/include/radixtree.h
+++ src/data_structure/radix_tree/include/radixtree.h
@@ -1,9 +1,15 @@
 #ifndef RADIXTREE_H
 #define RADIXTREE_H
 
+#include <stdint.h>
+
 typedef struct radixtree radixtree;
 
 radixtree *radixtree_init(void);
 void radixtree_free(radixtree *tree);
+void radixtree_insert(radixtree *tree, uintptr_t key, void *value);
+void *radixtree_delete(radixtree *tree, uintptr_t key);
+void *radixtree_search(radixtree *tree, uintptr_t key);
+void radixtree_clear(radixtree *tree);
 
 #endif  // RADIXTREE_H
