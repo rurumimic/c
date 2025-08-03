4dd2d695c8df0a7a35b0881aa0c9e0d6d06495c8
rurumimic <10915235+rurumimic@users.noreply.github.com>
2025-08-03 21:52:28 +0900

    fix code style

--- src/data_structure/radix_tree/src/rdx_alloc.c
+++ src/data_structure/radix_tree/src/rdx_alloc.c
@@ -1,9 +1,5 @@
 #include "rdx_alloc.h"
 
-void *rdx_malloc(size_t size) {
-    return malloc(size);
-}
+void *rdx_malloc(size_t size) { return malloc(size); }
 
-void rdx_free(void *ptr) {
-    free(ptr);
-}
+void rdx_free(void *ptr) { free(ptr); }
--- src/data_structure/radix_tree/src/rdx_alloc.h
+++ src/data_structure/radix_tree/src/rdx_alloc.h
@@ -9,8 +9,8 @@
 #define rdx_free rdx_free
 #endif
 
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 void *rdx_malloc(size_t size);
 void rdx_free(void *ptr);
